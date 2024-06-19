139113e3cdf483982b984cab5035eebbfdbdf58c
Raphael Costa <67166884+raphaelcosta1@users.noreply.github.com>
2024-06-19 11:09:00 -0300

    Update script

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ logger = Logger.new(STDOUT)
 logger.datetime_format = '%Y-%m-%d %H:%M:%S'
 
 #Informações do Clockify
-api_key_clockify = 'your_clockify_api_key'
-email = 'your_email'
+api_key_clockify = 'MDM3MzM4NDktYzRlYS00YzFlLWIxM2YtNTY2YWM1NzBhZGU3'
+email = 'raphaelcosta1.tech@gmail.com'
 
 # Instancia o RedmineConnector::Client com o logger
-api_key_redmine = 'your_redmine_api_key' 
+api_key_redmine = '790d421e34f6ca3c9a453d19e1abdf3bc725218a'
 connector = RedmineConnector::Client.new(api_key_clockify, api_key_redmine, email, logger)
 
 logger.info('Starting Job')
