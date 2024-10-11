af989ac326d2ea16aa641efff8a35a445dc77bff
Raphael Costa <67166884+raphaelcosta1@users.noreply.github.com>
2024-10-11 17:40:32 -0300

    Change schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,3 @@
-every 1.minute do
-# every 1.day, at: '06:15 pm' do
-# every :saturday, at: '12:00 am' do
+every :saturday, at: '12:00 am' do
     command "cd /home/rapha/Documentos/Projetos/apontamento_de_horas && /home/rapha/.rbenv/shims/ruby /home/rapha/Documentos/Projetos/apontamento_de_horas/app.rb >> /home/rapha/Documentos/Projetos/apontamento_de_horas/logs/apontamento_de_horas.log 2>&1"
 end
